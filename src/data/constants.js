520f7fbe30e2a0f7632040a29b4e0a2e1b6e47ba
Lucas Marujo <lucas.m.amadeu@icloud.com>
2024-12-06 08:35:50 -0500

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,322 @@
+import portfolioImage from './portfolio.png';
+import handImage from './handtracker.png';
+import masclubImage from './masclub.png';
+import adapitfy from './adapitfy.png'
+
+export const Bio = {
+  name: "Lucas Marujo",
+  roles: [
+    "FrontEnd Developer",
+    "UI/UX Web Designer",
+    "SAAS Entrepreneur"
+  ],
+  description:
+    "I'm a motivated and versatile person, always willing to take on new challenges. With a passion for learning, I'm dedicated to delivering high-quality results. With a positive attitude and a growth mindset, willing to contribute significantly and achieve great things.",
+  github: "https://github.com/lucasmarujo",
+  resume:
+    "https://drive.google.com/file/d/1QH-MBrgXimfvk_Vv3Bduwc0hsSXdeVXs/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/lucas-marujo-amadeu-5322a7219/",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/lucas.marujo/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Front-End",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://github.com/tandpfun/skill-icons/raw/main/icons/HTML.svg",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/CSS.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/JavaScript.svg",
+      },
+      {
+        name: "TypeScript",
+        image: "https://github.com/tandpfun/skill-icons/raw/main/icons/TypeScript.svg",
+      },
+      {
+        name: "React Js",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/React-Dark.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/NextJS-Dark.svg",
+      },
+      
+    ],
+  },
+  {
+    title: "Back-End",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://github.com/tandpfun/skill-icons/raw/main/icons/NodeJS-Dark.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/Python-Dark.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/MySQL-Dark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/MongoDB.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://github.com/tandpfun/skill-icons/raw/main/icons/Firebase-Dark.svg",
+      },
+      
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/Github-Dark.svg",
+      },
+      {
+        name: "Git",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/Git.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/VSCode-Dark.svg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/Figma-Dark.svg",
+      },
+      {
+        name: "AutoCad",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/AutoCAD-Dark.svg",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/AWS-Dark.svg",
+      },
+      {
+        name: "Azure",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/Azure-Dark.svg",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://github.com/tandpfun/skill-icons/raw/main/icons/Linux-Dark.svg",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/D4D0BAQGb6at2pVKxRA/img-crop_100/img-crop_100/0/1722603872859?e=1738195200&v=beta&t=7dkuIsW_LUO344q6lyl-_cFUMppdCNAYZ_esM4w9qKc",
+    role: "Registration assistant promoted to electronics maintenance technician",
+    company: "GAG Reciclagem de Eletronicos",
+    date: "Sep 2023 - Sep 2024",
+    desc: "I was responsible for entering products into the system and testing hardware, dealing with various types of hardware, such as notebooks, Android and iPhones, MacBooks, Cisco routers, Oracle servers, switches and firewalls. I performed tests and maintenance, and collaborated with the receiving and shipping departments to meet customer needs and provide the requested reports.",
+    skills: [
+      "Notebook maintenance and repair",
+      "Cell phone maintenance and repair",
+      "Test and configuration of MacBook's",
+      "Data deletion from hard disks and SSD's",
+    
+    ],
+    },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/v2/C4D0BAQFK07MyyVk3HA/company-logo_200_200/company-logo_200_200/0/1680024356795/boa_supermercados_logo?e=1738195200&v=beta&t=06K3S0v1tR-CeuAh8XKcXDeJyDVt95AHkevD4_wsWM8",
+    role: "Intern in IT Support",
+    company: "Boa Supermercados",
+    date: "Oct 2022 - Jan 2023",
+    desc: "Network maintenance, software and hardware, including physical and logical maintenance on the PDV systems of each supermarket branch and other sectoral systems. High knowledge in ERP Protheus, maintenance coded in Linux, as well as indications and resolution of failures related to hardware and systems used.",
+    skills: [
+      "Network",
+      "PVD",
+      "Protheus ERP",
+      "Linux",
+    ],
+    },
+];
+
+export const education = [
+  
+ 
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C560BAQEV_MO22Fe1bQ/company-logo_100_100/company-logo_100_100/0/1630608213891/unianchieta_logo?e=1738195200&v=beta&t=ChHxqvwsoRihazPzXdohzjvwKeUuO_1vYPwrNk-sfd4",
+    school: "University Padre Anchieta - SP Brazil",
+    date: "Jan 2021 - Dec 2025",
+    grade: "In Progress",
+    desc: "Currently in the seventh semester.",
+    degree: "Bachelor's Degree - Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/v2/D4E0BAQFloaXIKYL6kw/company-logo_200_200/company-logo_200_200/0/1719941469307/ilac_logo?e=1741219200&v=beta&t=h4xIaOJ-bisqmCR3Q8UHeyDFcJp3labvDxObQSGIXfk",
+    school: "ILAC",
+    date: "Nov 2024 - Dec 2024",
+    grade: "74%",
+    desc: "Cambridge English Level High-Advanced - CAE C1+",
+    degree: "International English Intensive",
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/v2/C4D0BAQHQaa7Z4TUWcQ/company-logo_100_100/company-logo_100_100/0/1630565324614/faculdadeflamingo_logo?e=1738195200&v=beta&t=sgOXzI43VVo_qImnEojz6smUrA2EKU9ss8sriCWuxhE",
+    school: "College Flamingo - SP Brazil",
+    date: "jan 2019 - Dec 2021",
+    grade: "95.2%",
+    desc: "Completed high school at the university's school, simultaneously studying Computer Technician, located in Lapa in São Paulo",
+    degree: "High School + Technician",
+  },
+  {
+    id: 3,
+    img: "https://scontent-yyz1-1.xx.fbcdn.net/v/t39.30808-6/464684351_1071939684850622_1101791995020646538_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=c3ZujaM1XG0Q7kNvgFo4kHH&_nc_zt=23&_nc_ht=scontent-yyz1-1.xx&_nc_gid=Arbi1qXfGyx7Q0aAsJUQKv5&oh=00_AYBU1hT77gBe1lTMd3r-TnW7182R7Q3hvASyl0Ey9Zwa6g&oe=675669A5",
+    school: "Colégio Cosmos",
+    date: "Jan 2009 - Dec 2018",
+    grade: "87.3%",
+    desc: "Completed elementary school at the school, also participated in extracurricular activities that allowed me to have a wide variety of cultures.",
+    degree: "Elementary School",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Adapitfy ",
+    date: "Early 2025",
+    description:
+      "A SAAS developed individually by me, with AI services available, sales assistance, 24-hour chat support. Initially it's a project for startups or medium-sized companies, but it can also be used by individuals.",
+    image: adapitfy,
+    tags: [
+      "Next Js",
+      "Tailwind",
+      "Clerk",
+      "MySQL",
+      "Python",
+      "Typescript"
+    ],
+    category: "backend",
+    github: "https://github.com/lucasmarujo",
+    member: [
+      {
+        name: "Lucas Marujo Amadeu",
+        img: "https://avatars.githubusercontent.com/u/97917029?v=4",
+        linkedin: "https://www.linkedin.com/in/lucas-marujo-amadeu-5322a7219/",
+        github: "https://github.com/lucasmarujo",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Portfolio",
+    date: "Nov 2024",
+    description:
+      "This project focuses on improving my frontend skills. Was developed in two different languages with the aim of giving more visibility and greater networking possibilities with people from other countries with the same interest.",
+    image: portfolioImage,
+    tags: [
+      "React Js",
+      "Node Js",
+      "Js",
+      "TypeScript",
+    ],
+    category: "frontend",
+    github: "https://github.com/lucasmarujo/portfolio",
+  },
+  {
+    id: 2,
+    title: "HandTracking",
+    date: "Sep 2024",
+    description:
+      "Using some basic framework libraries, together with the WebCam hardware, it is possible to track hands with specific points (OpenCV, MediaPipe).",
+    image: handImage,
+    tags: ["Python"],
+    category: "backend",
+    github: "https://github.com/lucasmarujo/handtrack-project",
+    
+  },
+  {
+    id: 2,
+    title: "MasClub",
+    date: "Jul 2024",
+    description:
+      "MasClub is a Sneaker's and clothing e-commerce that was developed in partnership with a colleague from the Computer Science course, using lowcode frameworks and a payment system. We have kept the website integrated and functional up to the present moment. We used DROPSHIPPING as a strategy and made direct sales with the supplier, importing a certain product from China to Brazil, according to the customer's choice. Along with the website, we developed an application that downloads all the product images into a folder, using python",
+    image:
+      masclubImage,
+    tags: ["Yampi", "ZoHo", "Python"],
+    category: "backend",
+    webapp: "https://masclub.catalog.yampi.io/",
+    member: [
+      {
+        name: "Lucas Marujo Amadeu",
+        img: "https://avatars.githubusercontent.com/u/97917029?v=4",
+        linkedin: "https://www.linkedin.com/in/lucas-marujo-amadeu-5322a7219/",
+        github: "https://github.com/lucasmarujo",
+      },
+      {
+        name: "Lucas Ferreira Balduino",
+        img: "https://avatars.githubusercontent.com/u/143102979?v=4",
+        linkedin: "https://www.linkedin.com/in/lucas-ferreira-balduino-a7a960227/",
+        github: "https://github.com/Lucaskjkk",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Job Finder",
+    date: "Jan 2025",
+    description:
+      "This project is a Python-based bot developed independently, designed to streamline job application processes by automating CV submissions on job platforms. Leveraging web scraping techniques and other advanced programming tools, the bot efficiently navigates job boards, fills out application forms, and uploads the CV to relevant job postings.",
+    image: handImage,
+    tags: ["Python"],
+    category: "machine-learning",
+    github: "https://github.com/lucasmarujo/handtrack-project",
+    
+  },
+  {
+    id: 2,
+    title: "CRUD with swagger+docker",
+    date: "Dec 2024",
+    description:
+      "This project is a full-stack CRUD application developed independently, designed to manage data with a focus on scalability, documentation, and deployment. The application was built using modern frameworks and tools, including Swagger for API documentation and Docker for containerization.",
+    image: handImage,
+    tags: ["Node Js", "Swagger", "Docker"],
+    category: "machine learning",
+    github: "https://github.com/lucasmarujo/handtrack-project",
+    
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
